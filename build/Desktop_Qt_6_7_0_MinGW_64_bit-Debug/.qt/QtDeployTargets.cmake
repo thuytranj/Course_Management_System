5e5bf11eb8bfa3f9d0456b3c2d2b6c3eb676aca3
Thuy Tran <158675897+thuytranj@users.noreply.github.com>
2024-06-11 06:28:11 +0700

    Delete build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Course_Management_System_FILE D:/BONUS PROJECT/Course_Management_System/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/Course_Management_System.exe)
-set(__QT_DEPLOY_TARGET_Course_Management_System_RUNTIME_DLLS D:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
